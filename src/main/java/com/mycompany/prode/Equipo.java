7f0774b23d841bd70ae57fcbb85994544d2ae217
Dario Lopez <26413@sistemas.frc.utn.edu.ar>
2023-04-28 02:11:10 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/prode/Equipo.java
@@ -0,0 +1,36 @@
+
+package com.mycompany.prode;
+
+
+public class Equipo {
+  private String nombre;
+ private String Descripcion;
+    public Equipo() {
+    }
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+      
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+   
+
+    
+ 
+}
